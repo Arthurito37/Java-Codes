db9172b0effbf513acffa8ff25e5311a9a3a98bd
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2024-05-11 16:22:39 -0300

    Aula 2 Java SENAI

---
+++ logica-de-programacao/src/tipos/primitivos/Inicio.java
@@ -0,0 +1,17 @@
+package tipos.primitivos;
+
+public class Inicio {
+
+    public static void main(String[]args){
+        byte idadeUsuario = 27;
+        String nomeUsuario = "Arthur de Sousa Santana";
+        double alturaUsuario = 1.77;
+        System.out.println("Olá.");
+        System.out.println("Meu nome é Arthur de Sousa Santana.");
+        System.out.println("Tenho " + alturaUsuario + ".");
+        System.out.println("Tenho " + idadeUsuario+ " de idade.");
+        System.out.println("Serei programador.");
+
+    }
+}
+    
