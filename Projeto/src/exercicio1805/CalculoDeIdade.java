069d680cf7471d82fdafef79d65a8d8c8174b3fe
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2024-05-18 15:45:01 -0300

    Update CalculoDeIdade.java
    Atualização na data, onde utilizo a data atual com a função LocalDate

--- Projeto/src/exercicio1805/CalculoDeIdade.java
+++ Projeto/src/exercicio1805/CalculoDeIdade.java
@@ -1,20 +1,24 @@
 package exercicio1805;
+import java.time.LocalDate;
 import java.util.Scanner;
 public class CalculoDeIdade {
     public static void main(String[] args) {
         /**
-         * Este código em JAVA faz o calculo de idade com base no ano atual e na data de nascimento.
+         * Este código em JAVA faz o calculo de idade com base na data atual e na data de nascimento.
          *
          * Data: 18/05/2024
          *
          * Criado por Arthur de Sousa Santana
          **/
         Scanner sc = new Scanner(System.in);
+        LocalDate data = LocalDate.now();
         System.out.println("Em qual ano você nasceu?");
         int dataNasc = sc.nextInt();
-        final int anoAtual = 2024;
+        int anoAtual;
+        anoAtual = LocalDate.now().getYear();
         int idadeAtual = anoAtual - dataNasc;
         System.out.println("Com a data de hoje 18/05/2024 você tem "+ idadeAtual +" anos de idade.");
         sc.close();
     }
 }
+
