aac81495f18b244151a2d636a505c71fa3a68107
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2024-08-07 00:36:03 -0300

    Add files via upload

---
+++ beecrowd/src/exercicioBeecrowd/conversaoDeTempo.java
@@ -0,0 +1,28 @@
+package exercicioBeecrowd;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class conversaoDeTempo {public static void main(String[] args) throws IOException {
+    Scanner scanner = new Scanner(System.in);
+    int N = scanner.nextInt();
+    int horas = 0;
+    int minutos = 0;
+    int segundos = 0;
+    if (N >= 3600) {
+        while (N >= 3600){
+            N = N - 3600;
+            horas = horas + 1;
+        }
+    }
+    if (N < 3600 && N > 60) {
+        while (N >= 60) {
+            N = N - 60;
+            minutos = minutos + 1;
+        }
+    }
+    if (N < 60){
+        segundos = N;
+    }
+    System.out.println(horas + ":" + minutos + ":" + segundos);
+    }
+}
