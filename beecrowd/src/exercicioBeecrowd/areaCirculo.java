513a7695f359aefe6b82f1400db107ab4c835922
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2024-08-11 23:29:20 -0300

    Atualização com matrizes e comentários

--- beecrowd/src/exercicioBeecrowd/areaCirculo.java
+++ beecrowd/src/exercicioBeecrowd/areaCirculo.java
@@ -6,12 +6,17 @@ public class areaCirculo{
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         final double n = 3.14159;
+        System.out.println("Digite o raio do círculo:");
+        
+        // cria a matriz que recebe o dados
+        double [] i = new double[2];
+        // recebe o raio
+        i [0] = scanner.nextDouble();
+        // armaneza o valor da área
+        i [1] = n * (i [0] * i [0]);
 
-        double raio = scanner.nextDouble();
-        double area = n * (raio * raio);
+        System.out.printf("A=%.4f%n", i [1]);
 
-        System.out.printf("A=%.4f%n", area);
-
-        scanner.close();
+        scanner.close(); // Boa prática para fechar o scanner
     }
 }
