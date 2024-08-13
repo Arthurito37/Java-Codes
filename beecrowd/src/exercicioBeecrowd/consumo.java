9f28f44329624c292dad5cd0cbb5ec726af52fc4
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2024-08-13 20:52:28 -0300

    Atualização com matrizes e comentários

--- beecrowd/src/exercicioBeecrowd/consumo.java
+++ beecrowd/src/exercicioBeecrowd/consumo.java
@@ -1,19 +1,19 @@
 package exercicioBeecrowd;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class consumo {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        double [] valores = new double [3]; //declara a matriz que recebe os valores
         System.out.println("Digite a distância percorrida:");
-        double km = scanner.nextDouble();
+        valores [0] = scanner.nextDouble(); // recebe o valor responsável pela distância
         System.out.println("Digite a quantidade de litros consumida");
-        double l = scanner.nextDouble();
+        valores [1] = scanner.nextDouble(); // recebe o valor responsável pelos litro consumidos
 
-        double consumoMedio = km/l;
+        valores [2] = valores [0]/valores[1]; // faz o calculo responsável pela média dos quilômetros percorridos por litro
 
-        System.out.printf("FORAM PERCORRIDOS EM MÉDIA %.3f km/l%n", consumoMedio);
+        System.out.printf("FORAM PERCORRIDOS EM MÉDIA %.3f km por litro %n", valores[2]);
 
     scanner.close();
 
