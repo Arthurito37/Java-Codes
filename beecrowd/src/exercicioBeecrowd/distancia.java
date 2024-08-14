4b01d406021fd87915d9ef6cb7baaebe3dec1700
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2024-08-13 21:06:12 -0300

    Atualização com matrizes e comentários

--- beecrowd/src/exercicioBeecrowd/distancia.java
+++ beecrowd/src/exercicioBeecrowd/distancia.java
@@ -2,14 +2,14 @@ package exercicioBeecrowd;
 import java.io.IOException;
 import java.util.Scanner;
 public class distancia {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        final int velX = 60;
-        final int velY = 90;
-
-        int distancia = scanner.nextInt();
-        int tempo = (60*distancia)/30;
-        System.out.println(tempo + " minutos");
+        final int [] valores = new int [4]; //cria a matriz 
+        valores [0] = 60; // define a velocidade como 60
+        valores [1] = 90; // define a segunda velocidade como 90
+        valores [2] = scanner.nextInt();
+        valores [3] = (60*valores[2])/30; //faz o cálculo da distância entre os veiculos
+        System.out.println(valores[3] + " minutos");
         scanner.close();
     }
 }
