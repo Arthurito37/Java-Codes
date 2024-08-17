a5cb9d932ee1608364f4e3e5f3cf365dd61117e9
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2024-08-17 19:33:32 -0300

    Atualização com matrizes e comentários

--- beecrowd/src/exercicioBeecrowd/esfera.java
+++ beecrowd/src/exercicioBeecrowd/esfera.java
@@ -5,12 +5,13 @@ public class esfera {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         final double pi = 3.14159;
+        double [] dados = new double [3]; //armazena os dados
         System.out.println("Qual o valor do raio do círculo?");
-        double r = scanner.nextInt();
+        dados [0] = scanner.nextDouble(); //recebe o dados do usuário
 
-        double volume = (4.0/3) * pi * ((r*r)*r);
+        dados [1] = (4.0/3) * pi * ((dados [0]*dados [0])*dados [0]); //realiza o calculo do volume
 
-        System.out.printf("O VOLUME  DO CÍRCULO É IGUAL A %.3f%n", volume);
+        System.out.printf("O VOLUME  DO CÍRCULO É IGUAL A %.3f%n", dados [1]); //retorna ao usuario o calculo do volume
 
         scanner.close();
     }
