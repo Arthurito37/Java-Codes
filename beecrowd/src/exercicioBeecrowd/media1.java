76605b0e5a1a759496fe38ae6b475d8387c064d7
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2024-08-17 20:00:01 -0300

    Atualização com matrizes e comentários

--- beecrowd/src/exercicioBeecrowd/media1.java
+++ beecrowd/src/exercicioBeecrowd/media1.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 public class media1 {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
+        final double [] peso = new double [3]; //array que armazenas o pesos que não são alterados
+        double [] nota = new double [5]; //array que armazena as notas, a soma das notas e média final
+        peso [0] = 3.5;  //atribui o peso da nota
+        peso [1] = 7.5;  //atribui o peso da nota
+        System.out.println("Digite a sua nota:");
+        nota [0] = scanner.nextDouble(); //solicita ao usuário o peso da nota
+        System.out.println("Digite a sua nota:");
+        nota [1] = scanner.nextDouble(); //solicita ao usuário o peso da nota
 
-        final double peso1 = 3.5;
-        final double peso2 = 7.5;
+        nota [3] = peso [0] + peso [1]; //soma as duas notas
 
-        double nota1 = scanner.nextDouble();
-        double nota2 = scanner.nextDouble();
+        nota [0] = nota [0] * peso [0]; //faz os calculos da primeira nota com o peso
+        nota [1] = nota [1] * peso [1]; //faz os calculas da segunda nota com o peso
 
-        double somaPeso = peso1 + peso2;
-
-        nota1 = nota1 * peso1;
-        nota2 = nota2 * peso2;
-
-        double MEDIA = ((nota1 + nota2)/somaPeso);
-        System.out.printf("MEDIA = %.5f%n", MEDIA);
-
-
+        nota [4]= ((nota [0] + nota [1])/nota [3]); //faz o calculo da média
+        System.out.printf("MEDIA = %.5f%n", nota [4]); //retorna a informação ao usuário
     }
 }
