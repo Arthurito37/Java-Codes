57a421cfefcf8c4d808fdeeb583299b9c8198839
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2024-08-17 19:39:56 -0300

    Atualização com matrizes e comentários

--- beecrowd/src/exercicioBeecrowd/gastoDeCombustivel.java
+++ beecrowd/src/exercicioBeecrowd/gastoDeCombustivel.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 public class gastoDeCombustivel {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-
-        int horas = scanner.nextInt();
-        int velocidade = scanner.nextInt();
-        int distancia = horas*velocidade;
+        double [] dados= new double [4]; //armazena os dados
         final double eficiencia = 12.0;
-        double gastoCombustivel = distancia/eficiencia;
-        System.out.printf("%.3f%n", gastoCombustivel);
+        System.out.println("Quantas horas teve a viagem?");
+        dados [0] = scanner.nextDouble(); //recebe as horas
+        System.out.println("Qual foi a velocidade mantida durante a viagem?");
+        dados [1] = scanner.nextDouble(); //recebe a velocidade
+        dados [2] = dados [0] * dados [1];
+        dados [3] = dados [2]/eficiencia; //calcula o gasto do combustivel
+        System.out.printf("%.3f%n", dados [3]); //retorna os dados ao usuário
     }
 }
