c641f8424fdf48b57cfb4d9f262e888f02d55a37
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2024-08-17 22:13:28 -0300

    Atualização com matrizes e comentários

--- beecrowd/src/exercicioBeecrowd/somaSimples.java
+++ beecrowd/src/exercicioBeecrowd/somaSimples.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class somaSimples {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
+        int [] numeros = new int [4];
+        System.out.println("Digite a primeira parcela:");
+        numeros [0] = scanner.nextInt();
+        System.out.println("Digite a segunda parcela:");
+        numeros [1] = scanner.nextInt();
 
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-
-        int soma = a + b;
-        System.out.printf("SOMA = " + soma);
+        numeros [2] = numeros [0] + numeros [1];
+        System.out.printf("SOMA = " + numeros [2]);
     }
 }
