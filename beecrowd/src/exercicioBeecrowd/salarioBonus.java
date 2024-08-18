acc9470f8ca68563a4b793260934d1f5a71102ec
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2024-08-17 22:10:12 -0300

    Atualização com matrizes e comentários

--- beecrowd/src/exercicioBeecrowd/salarioBonus.java
+++ beecrowd/src/exercicioBeecrowd/salarioBonus.java
@@ -4,18 +4,21 @@ import java.util.Scanner;
 public class salarioBonus {
     public static void main(String[] args) throws IOException {
     Scanner scanner = new Scanner(System.in);
+    String [] name = new String [1];
+    double [] valores = new double [4];
     System.out.println("Digite o nome do vendedor:");
-    String name = scanner.nextLine();
+    name [0] = scanner.nextLine();
     System.out.println("Qual é o salário fixo do funcionário:");
-    double salary = scanner.nextDouble();
+    valores [0] = scanner.nextDouble();
     System.out.println("Quantas vendas o funcionário realizou (Em Dinheiro)?");
-    double sales = scanner.nextDouble();
+    valores [1] = scanner.nextDouble();
 
-    double porc = sales * 0.15;
+    valores [2] = valores [1] * 0.15;
 
-    double total = salary + porc;
+    valores [3] = valores [0] + valores [2];
 
-    System.out.printf("O valor total que o vendedor irá receber é R$ %.2f%n", total);
+    System.out.printf("O valor total que o vendedor irá receber é R$ %.2f%n", valores [3]);
     scanner.close();
 }
 }
+
