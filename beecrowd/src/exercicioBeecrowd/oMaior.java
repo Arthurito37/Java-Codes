0adab55ed909eece48f0ac01df7962c8947c0239
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2024-08-17 21:30:40 -0300

    Atualização com matrizes e comentários

--- beecrowd/src/exercicioBeecrowd/oMaior.java
+++ beecrowd/src/exercicioBeecrowd/oMaior.java
@@ -4,19 +4,21 @@ import java.io.IOException;
 
 public class oMaior {public static void main(String[] args) throws IOException {
     Scanner scanner = new Scanner(System.in);
+    int [] numeros = new int [6];
     System.out.println("Digite um número:");
-    int a = scanner.nextInt();
+    numeros [0] = scanner.nextInt();
     System.out.println("Digite outro número:");
-    int b = scanner.nextInt();
+    numeros [1] = scanner.nextInt();
     System.out.println("Digite mais um número:");
-    int c = scanner.nextInt();
+    numeros [2] = scanner.nextInt();
     // A fórmula usa Math.abs para encontrar a diferença absoluta entre a e b
     // e então encontrar o maior valor entre a e b.
-    int MaiorAB = (a + b + Math.abs(a-b) ) / 2;
+    numeros [3] = (numeros [0] + numeros [1] + Math.abs(numeros [0]-numeros [1]) ) / 2;
     //A fórmula usa Math.abs para encontrar a diferença absoluta entre maiorAB e c
     // e então encontrar o maior valor entre maiorAB e c.
-    int MaiorABC = (MaiorAB + c + Math.abs(MaiorAB - c)) / 2;
-    System.out.println(MaiorABC + " e o maior número digitado.");
+    numeros [4] = (numeros [3] + numeros [2] + Math.abs(numeros [3] - numeros [2])) / 2;
+    System.out.println(numeros [4] + " e o maior número digitado.");
     scanner.close();
     }
 }
+
