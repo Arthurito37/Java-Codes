bd7183f283f9cc3a143ea414a78abef5550b1e25
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2024-08-17 21:58:17 -0300

    Atualização com matrizes e comentários

--- beecrowd/src/exercicioBeecrowd/salario.java
+++ beecrowd/src/exercicioBeecrowd/salario.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 public class salario {
     public static void main(String[] args) throws IOException {
     Scanner scanner = new Scanner(System.in);
+    int [] inteiros = new int [3]; // armazena o código do funcionário e a hora
+    double [] real = new double [3]; //armazena o valor da hora e do salário
     System.out.println("Digite o código do fuuncionário:");
-    int number = scanner.nextInt();
+    inteiros [0] = scanner.nextInt(); //recebe a código do trabalhador
     System.out.println("Digite as horas trabalhadas pelo funcionário:");
-    int hours = scanner.nextInt();
+    inteiros [1] = scanner.nextInt(); //recebe as horas trabalhadas
     System.out.println("Quanto é o valor da hora do funcionário?");
-    double valueHour = scanner.nextDouble();
+    real [0] = scanner.nextDouble(); //recebe o valor da hora trabalhada
 
-    double salary = valueHour * hours;
+    real [1] = real [0] * inteiros [1];
 
-    System.out.println("O número do funcionário é: " + number);
-    System.out.printf("O funcionário irá receber U$ %.2f%n ", salary);
+    System.out.println("O número do funcionário é: " + inteiros [0]);
+    System.out.printf("O funcionário irá receber U$ %.2f%n ", real [1]);
 
     scanner.close();
 }
