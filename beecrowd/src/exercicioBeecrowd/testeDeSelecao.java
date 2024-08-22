298174e43bf1eb709d5c06646268c0e8b942c89c
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2024-08-21 23:28:21 -0300

    Em breve atualização com matrizes e comentários

---
+++ beecrowd/src/exercicioBeecrowd/testeDeSelecao.java
@@ -0,0 +1,40 @@
+package exercicioBeecrowd;
+import java.util.Scanner;
+public class testeDeSelecao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("DIGITE UM NÚMERO:");
+        int A = scanner.nextInt();
+        System.out.println("DIGITE OUTRO NÚMERO:");
+        int B = scanner.nextInt();
+        System.out.println("DIGITE OUTRO NÚMERO:");
+        int C = scanner.nextInt();
+        System.out.println("DIGITE O ÚLTIMO NÚMERO:");
+        int D = scanner.nextInt();
+        int par = A % 2;
+        int somaAeB = A + B;
+        int somaCeD = C + D;
+        if (C >= 0 && D >= 0) {
+            if (B > C) {
+                if (D > A) {
+                    if (somaCeD > somaAeB) {
+                        if (par == 0) {
+                            System.out.println("Valores aceitos");
+                        } else {
+                            System.out.println("Valores nao aceitos");
+                        }
+                    } else {
+                        System.out.println("Valores nao aceitos");
+                    }
+                } else {
+                    System.out.println("Valores nao aceitos");
+                }
+            } else {
+                System.out.println("Valores nao aceitos");
+            }
+        } else {
+            System.out.println("Valores nao aceitos");
+        }
+        scanner.close();
+    }
+}
