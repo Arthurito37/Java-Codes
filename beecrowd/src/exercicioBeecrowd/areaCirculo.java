bd31bf7e3c2f66ee01bdd5dc367ea69b2c2cae97
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2024-08-23 22:50:29 -0300

    Atualização com caixa de diálogos

--- beecrowd/src/exercicioBeecrowd/areaCirculo.java
+++ beecrowd/src/exercicioBeecrowd/areaCirculo.java
@@ -1,4 +1,5 @@
 package exercicioBeecrowd;
+import javax.swing.*;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -6,17 +7,19 @@ public class areaCirculo{
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         final double n = 3.14159;
-        System.out.println("Digite o raio do círculo:");
-        
         // cria a matriz que recebe o dados
-        double [] i = new double[2];
+        double [] i = new double[4];
         // recebe o raio
-        i [0] = scanner.nextDouble();
-        // armaneza o valor da área
-        i [1] = n * (i [0] * i [0]);
-
-        System.out.printf("A=%.4f%n", i [1]);
-
-        scanner.close(); // Boa prática para fechar o scanner
+        while (i [2] == 0){
+            i [0] = Float.parseFloat(JOptionPane.showInputDialog("Digite o raio do círculo:")); // armazena o valor e cria uma caixa de diálogo
+            // armaneza o valor da área
+            i [1] = n * (i [0] * i [0]);
+            JOptionPane.showMessageDialog(null, "A área do círculo é: " + i[1]); // retorna um diálogo e a área do circulo
+            i [3] = JOptionPane.showConfirmDialog(null, String.format("Deseja realizar mais calculos de área?"), "Calculo de Área",JOptionPane.YES_NO_OPTION); 
+            if (i [3] == JOptionPane.NO_OPTION) {
+                JOptionPane.showMessageDialog(null, "Ok, tenha um ótimo dia.");
+                i[2]++;
+            }
+         }
     }
 }
