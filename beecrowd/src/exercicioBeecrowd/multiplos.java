e91020952e5a9b5e1ad31f9355efd0bb16304e71
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2024-08-28 23:13:59 -0300

    Add files via upload

---
+++ beecrowd/src/exercicioBeecrowd/multiplos.java
@@ -0,0 +1,39 @@
+package exercicioBeecrowd;
+import java.util.Scanner;
+
+
+
+/**
+
+ * Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos",
+
+ * indicando se os valores lidos são múltiplos entre si.
+
+ *
+
+ * Data: 28/08/2024
+
+ * Atualizado em: 28/08/2024
+
+ * Criado por Arthur de Sousa Santana
+
+ **/
+
+public class multiplos {  public static void main(String[]args){
+    Scanner scanner = new Scanner(System.in);
+    int A = scanner.nextInt();
+    int B = scanner.nextInt();
+        if (A % B == 0 || B % A == 0) {
+            System.out.println("Sao Multiplos");
+
+        } else {
+
+            System.out.println("Nao sao Multiplos");
+
+        }
+
+        scanner.close();
+
+    }
+
+}
